e7ac6396c3150f3ea88cd08f889537387c405c6e
ArunKumar <arunsubramaniak@gmail.com>
2026-04-24 13:20:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('Hello from Arun - DevOps Pipeline!\n');
+  res.end('Hello from Arunak - DevOps Pipeline!\n');
 });
 server.listen(3000, () => console.log('App running on port 3000'));
